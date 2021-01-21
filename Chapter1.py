6856c139125de95d112cc45cc427a36e2dc4451c
Sangjoon Jung <sangjoonjung@yahoo.com>
2021-01-20 20:52:12 -0800

    Add webcam source

--- Chapter1.py
+++ Chapter1.py
@@ -1,12 +1,24 @@
 import cv2
 print("Package Imported")
-
+"""
 img = cv2.imread("Resources/lena.jpg")
 
 cv2.imshow("Output", img)
 cv2.waitKey(0)  #0 infinite delay, 1000 -> 1sec
 
 cap = cv2.VideoCapture("Resources/test_video.mp4")
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+"""
+
+#Web Cam Access
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
 
 while True:
     success, img = cap.read()
